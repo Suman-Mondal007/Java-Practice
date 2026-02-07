5d4ef40de6967a23325a12c9a854797e10a91ae3
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-02-07 18:07:39 +0530

    sum

---
+++ first.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter value of a:");
+        int a =sc.nextInt();
+        System.out.println("Enter value of b:");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("Sum="+sum);
+    }
+}
