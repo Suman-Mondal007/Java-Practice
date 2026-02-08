17307e7136d6c6dc1c19d9de6756638829504721
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-02-09 01:27:32 +0530

    searching no index(2D)

---
+++ twoDArray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class twoDArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter rows:");
+        int rows=sc.nextInt();
+        System.out.println("Enter cols:");
+        int cols=sc.nextInt();
+        System.out.println("Enter array element:");
+        int arr[][]=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Enter elemnt to search:");
+        int search=sc.nextInt();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(arr[i][j]==search){
+                    System.out.println("Elemnt found at index:"+i+","+j);
+                }
+            }
+        }
+    }
+}
