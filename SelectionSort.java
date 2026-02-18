c499850737e5ae1c3fae236365936e9e326189a7
Suman-Mondal007 <145131739+Suman-Mondal007@users.noreply.github.com>
2026-02-19 00:00:21 +0530

    selection Sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 import java.util.*;
-public class BubbleSort {
+public class SelectionSort {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the range:");
@@ -9,13 +10,15 @@ public class BubbleSort {
             System.out.print("Enter the "+(i+1)+" no:");
             arr[i]=sc.nextInt();
         }
+
         for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(arr[j]<arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
+            for (int j=i+1;j<n;j++){
+               int smallest=i;
+               if(arr[smallest]>arr[j]){
+                   int temp=arr[smallest];
+                   arr[smallest]=arr[j];
+                   arr[j]=temp;
+               }
             }
         }
         System.out.println("Sorted array:");
